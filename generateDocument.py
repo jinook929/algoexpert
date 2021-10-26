988753076699db18e1eb0a5eef8ca9d2abd6bffb
jinook929 <jinook929@gmail.com>
2021-10-25 23:11:10 -0400

    implemented updated solution

--- generateDocument.py
+++ generateDocument.py
@@ -1,19 +1,32 @@
+# def generateDocument(characters, document):
+#   chars = {}
+#   docs = {}
+#   docset = set()
+
+#   for ch in characters:
+#     chars[ch] = chars.get(ch, 0) + 1
+
+#   for ch in document:
+#     docs[ch] = docs.get(ch, 0) + 1
+#     docset.add(ch)
+
+#   for ch in docset:
+#     if ch not in chars or chars[ch] < docs[ch]:
+#       return False
+
+#   return True
+
 def generateDocument(characters, document):
-  chars = {}
-  docs = {}
-  docset = set()
-
+  charcounts = {}
+  
   for ch in characters:
-    chars[ch] = chars.get(ch, 0) + 1
-
+    charcounts[ch] = charcounts.get(ch, 0) + 1
+    
   for ch in document:
-    docs[ch] = docs.get(ch, 0) + 1
-    docset.add(ch)
-
-  for ch in docset:
-    if ch not in chars or chars[ch] < docs[ch]:
+    if ch not in charcounts or charcounts[ch] == 0:
       return False
-
+    charcounts[ch] -= 1
+  
   return True
 
 characters = "Bste!hetsi ogEAxpelrt x "
