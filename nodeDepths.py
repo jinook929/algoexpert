220452d3d3d2ba737bd45eabd91ab9bf3d8a07b2
jinook929 <jinook929@gmail.com>
2021-11-03 23:55:33 -0400

    tried creating tree

--- nodeDepths.py
+++ nodeDepths.py
@@ -3,64 +3,62 @@ def nodeDepths(root):
     return
 
 class BinaryTree:
-    def __init__(self, value):
+    def __init__(self, node_id, value):
+        self.id = node_id
         self.value = value
         self.left = None
         self.right = None
 
-    def insert_left(self, value):
-        self.left = BinaryTree(value)
+    def insert_left(self, node):
+        self.left = node
 
-    def insert_right(self, value):
-        self.right = BinaryTree(value)
+    def insert_right(self, node):
+        self.right = node
 
-    def print_tree(self):
-        print(self.value)
+nodes = [
+    {"id": "1", "left": "2", "right": "3", "value": 1},
+    {"id": "2", "left": "4", "right": "5", "value": 2},
+    {"id": "3", "left": "6", "right": "7", "value": 3},
+    {"id": "4", "left": "8", "right": "9", "value": 4},
+    {"id": "5", "left": None, "right": None, "value": 5},
+    {"id": "6", "left": None, "right": None, "value": 6},
+    {"id": "7", "left": None, "right": None, "value": 7},
+    {"id": "8", "left": None, "right": None, "value": 8},
+    {"id": "9", "left": None, "right": None, "value": 9}
+]
+root = "1"
 
-case1 = {
-  "tree": {
-    "nodes": [
-      {"id": "1", "left": "2", "right": "3", "value": 1},
-      {"id": "2", "left": "4", "right": "5", "value": 2},
-      {"id": "3", "left": "6", "right": "7", "value": 3},
-      {"id": "4", "left": "8", "right": "9", "value": 4},
-      {"id": "5", "left": None, "right": None, "value": 5},
-      {"id": "6", "left": None, "right": None, "value": 6},
-      {"id": "7", "left": None, "right": None, "value": 7},
-      {"id": "8", "left": None, "right": None, "value": 8},
-      {"id": "9", "left": None, "right": None, "value": 9}
-    ],
-    "root": "1"
-  }
-}
+tmp = []
 
-# print(case1["tree"]["nodes"])
-nodes = case1["tree"]["nodes"]
-root = case1["tree"]["root"]
-# rootNode = list(filter(lambda item: item["id"] == root, nodes))[0]
-# print((rootNode))
+for i in range(len(nodes)):
+    tmp.append(BinaryTree(str(i + 1), nodes[i]["value"]))
 
 def findNode(id):
-    return list(filter(lambda item: item["id"] == id, case1["tree"]["nodes"]))[0]
-# print(findNode("9"))
+    return list(filter(lambda node: node.id == id, tmp))[0]
 
-# for node in case1["tree"]["nodes"]:
-#     if node["id"] == rootNode["id"]:
-#         root = BinaryTree(node["value"])
-#         # if root.left:
-#         #     root.insert_left(findNode(root.left))
-#         # if root.right:
-#         #     root.insert_right(findNode(root.right))
-# # root.print_tree()
+print(findNode("1"))
+print("=====")
+for i in reversed(range(len(tmp))):
+    print(nodes[i]["left"])
+    # if findNode(nodes[i]["left"]):
+    #     print(findNode(nodes[i]["left"]))
+    # if nodes[i]["left"]:
+    #     print(findNode(nodes[i]["left"]))
+        # tmp[i].left = findNode(nodes[i]["left"])
+    # if nodes[i]["right"]:
+    #     tmp[i].right = findNode(nodes[i]["right"])
 
-def createTree(root, nodes):
-    rootNode = list(filter(lambda node: node["id"] == root, nodes))[0]
-    trav = rootNode
-    # while not None:
-    while trav.left || trav.right:
-        if trav.left:
+# print(tmp[0].value)
+        
 
+# def findNode(id):
+#     return list(filter(lambda item: item["id"] == id, case1["tree"]["nodes"]))[0]
 
-    return rootNode
+# def createTree(root_id, nodes):
+#     root_obj = list(filter(lambda item: item["id"] == root_id, nodes))[0]
+#     root = BinaryTree(root_obj["value"])
+#     for obj in range(len(nodes)):
+#         if "left" in root_obj:
+#             findNode(root_obj["left"])
+        
 
-print(createTree(root, nodes))
